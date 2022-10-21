28253a85defccf151988a2e2c9a567737c1b8090
Bellemeeto <belle.n.meeto@gmail.com>
2022-10-21 00:28:48 -0400

    Hello World in Netbeans

---
+++ HelloWorld/src/main/java/com/belle/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Bellemeeto
+ *email: belle.n.meeto@gmail.com
+ *date: 10/21
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.belle.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
